ee35aa1adb6d34b891e6b78d80511d98798018df
Olaf Radicke <briefkasten@olaf-radicke.de>
2020-12-05 11:25:59 +0100

    Revoe variations.

--- functions/openssl_create_intermediate_ca_csr.sh
+++ functions/openssl_create_intermediate_ca_csr.sh
@@ -7,10 +7,6 @@ openssl_create_intermediate_ca_csr () {
   printf "/functions/openssl_create_intermediate_ca_csr.sh \n"
   printf "####################################### \n"
 
-  printf "#-------------------------------------# \n"
-  printf "With key format variation: ${HSM_SLOT}:000${ROOT_CA_KEY}\n"
-  printf "#-------------------------------------# \n"
-
   openssl req                                                                 \
     -new                                                                      \
     -sha256                                                                   \
@@ -20,21 +16,6 @@ openssl_create_intermediate_ca_csr () {
     -key ${HSM_SLOT}:000${ISSUE_CA_KEY}                                       \
     -keyform engine                                                           \
     -out ${DEMO_TMP_DIR}/issue_ca.csr.pem
-
-  printf "#-------------------------------------# \n"
-  printf "With key format variation: slot_${HSM_SLOT}-id_000${ROOT_CA_KEY}\n"
-  printf "#-------------------------------------# \n"
-
-
-  openssl req                                                                 \
-    -new                                                                      \
-    -sha256                                                                   \
-    -nodes                                                                    \
-    -config ${DEMO_CONFIG_DIR}/issue_ca/openssl.cnf                           \
-    -engine pkcs11                                                            \
-    -key slot_${HSM_SLOT}-id_000${ISSUE_CA_KEY}                               \
-    -keyform engine                                                           \
-    -out ${DEMO_TMP_DIR}/issue_ca-v2.csr.pem
 
   printf "#-------------------------------------# \n"
   printf "Check CSR file...\n"
